74e02246ba041eace04b6158be00023199ba365b
Lensikoviski <lensikoviski@gmail.com>
2022-12-05 06:54:36 +0530

    hosting

--- index.js
+++ index.js
@@ -2,15 +2,9 @@
 var express = require('express');
 var path = require('path')
 var {engine} = require('express-handlebars')
-const db = require('./database')
-
-
-
-
-
+const db = require('./database');
+const { resolve } = require('path');
 var app = express();
-
-
 app.engine('hbs',engine({extname:'hbs',defaultLayout:'default', layoutsDir: path.join(__dirname, '/views/layouts'),partialsDir:path.join(__dirname, 'views/partials')}))
 
 app.set('views', path.join(__dirname, 'views'));    
@@ -19,19 +13,26 @@ app.set('view engine','hbs')
 app.get('/user',(req,res)=>{
     res.render('layouts/user')
 })
-
-app.get('/home',async(req,res)=>{
-    await db.get().collection('userinfo').findOne({name:"akhil U Nair"}).then((data)=>{
-        if(data){
-            res.render('layouts/home',{data:data})
-        }
-    }).catch((err)=>{
-        res.send("An error occured")
-        console.log(err)
-    })
+app.get('/',async(req,res)=>{
+ 
+   
+  let data = await getdata()
+  res.render('layouts/home',{data:data})
+  console.log(data)    
    
 })
 
-const PORT = 3000  || process.env.PORT
 
+function getdata(){
+
+    return new Promise(async(resolve,reject)=>{
+        await db.get().collection('userinfo').findOne({name:'akhil U Nair'}).then((response)=>{
+           if(response){
+               resolve(response)
+           }
+        })
+   })
+
+}
+const PORT = 3000  || process.env.PORT
 app.listen(PORT)
