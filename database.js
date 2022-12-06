2daecb0f935dc47ab11592a55919b3d77b014da5
Lensikoviski <lensikoviski@gmail.com>
2022-12-06 18:36:18 +0530

    first commit

--- database.js
+++ database.js
@@ -1,18 +1,21 @@
 const mongoclient = require("mongodb").MongoClient
 const url = "mongodb+srv://jayaramskumar:just4marry@cluster0.gguofay.mongodb.net/matrimony?retryWrites=true&w=majority"
+console.log(url)
+console.log("hello")
 const state = {
     db:null
 }
-
+ 
 const dbname = 'matrimony'
 
 mongoclient.connect(url,(err,data)=>{
     if(err) throw err
-    state.db=data.db(dbname)
+    state.db=data.db(dbname) 
 })
 
 module.exports.get=()=>{
     return state.db
 }
+
 
 
--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var express = require('express');
 var path = require('path')
 var {engine} = require('express-handlebars')
 const db = require('./database');
-const { resolve } = require('path');
+const { resolve } = require('path'); 
 var app = express();
 app.engine('hbs',engine({extname:'hbs',defaultLayout:'default', layoutsDir: path.join(__dirname, '/views/layouts'),partialsDir:path.join(__dirname, 'views/partials')}))
 
-app.set('views', path.join(__dirname, 'views'));    
+app.set('views', path.join(__dirname, 'views'));     
 app.set('view engine','hbs') 
 
 app.get('/user',(req,res)=>{
