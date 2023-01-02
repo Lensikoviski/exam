07d64233f6f9e95ca2ea3936822899cbead9c4b3
Lensikoviski <lensikoviski@gmail.com>
2023-01-02 16:18:38 +0530

    changed to express-zip

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const app = express()
-const zip = require("express-easy-zip")
+const zip = require("express-zip")
 
-app.use(zip())
+//app.use(zip())
 const file = [
     {path:"./jayaram/file.c",name:"file.c"},
     {path:"./jayaram/new.c" ,name:"new.c"}
 ]
 app.get('/',(req,res)=>{
-    res.zip({files:file,filename:"jayaram.zip"})
+    res.zip(file)
 })
 
 app.listen(3000 || process.env.PORT)
